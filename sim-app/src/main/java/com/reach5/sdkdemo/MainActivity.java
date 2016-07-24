6af7b19c0c532850dfb504b2233b4e48b9b271d8
Nabil BENMIRA <nabenmir@yahoo.fr>
2016-07-24 14:25:33 +0200

    Add paypal dependency + logout operation

--- sim-app/src/main/java/com/reach5/sdkdemo/MainActivity.java
+++ sim-app/src/main/java/com/reach5/sdkdemo/MainActivity.java
@@ -10,7 +10,10 @@ import android.widget.TextView;
 import com.sim.sdk.R5Client;
 import com.sim.sdk.R5Exception;
 import com.sim.sdk.model.ProviderEnum;
+import com.sim.sdk.model.R5InitResponse;
 import com.sim.sdk.model.R5LoginResponse;
+
+import java.util.Arrays;
 
 public class MainActivity extends Activity implements R5Client.R5LoginListener{
 
@@ -24,7 +27,19 @@ public class MainActivity extends Activity implements R5Client.R5LoginListener{
 
         try {
             //initialize SDK
-            R5Client.init(this, "BvnynWPdlUOGwC3yERXi", "reach5.og4.me", null);
+            R5Client.initWithCallback(this, "9rnkdhnwC1nzKpQ3yFTE", "reach5-stg.og4.me", null, new R5Client.R5InitCallback() {
+                @Override
+                public void success(R5InitResponse response) {
+                    String providers = Arrays.toString(response.getProviders().toArray());
+                    Log.i("Activity", "Initialized providers = " + providers);
+                    ((TextView) findViewById(R.id.textView)).setText("Initialized providers = " + providers);
+                }
+
+                @Override
+                public void failure(R5Exception e) {
+                    showError(e);
+                }
+            });
             // register the login callback
             R5Client.registerLoginListener(this);
 
@@ -45,8 +60,7 @@ public class MainActivity extends Activity implements R5Client.R5LoginListener{
 
     @Override
     public void failure(R5Exception error) {
-        ((TextView) findViewById(R.id.textView))
-                .setText(String.format("Sim SDK Response Error !\n%s (code=%s)", error.getMessage(), error.getCode()));
+        showError(error);
         if (simSDKInitProgressBar != null) {
             simSDKInitProgressBar.dismiss();
             simSDKInitProgressBar = null;
@@ -90,11 +104,21 @@ public class MainActivity extends Activity implements R5Client.R5LoginListener{
                     simSDKInitProgressBar = ProgressDialog.show(MainActivity.this, "Please wait ...", "Contacting Paypal...", true);
                     R5Client.loginWithProvider(this, ProviderEnum.paypal, "homepage");
                     break;
+                case R.id.btn_logout:
+                    R5Client.logout();
+                    break;
             }
 
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    private void showError(R5Exception error) {
+        Log.e("Activity", error.getMessage(), error);
+        ((TextView) findViewById(R.id.textView))
+                .setText(String.format("R5 SDK Error !\n%s (code=%s)", error.getMessage(), error.getCode()));
+
     }
 
 }
