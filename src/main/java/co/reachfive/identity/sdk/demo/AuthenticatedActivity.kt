cb54153813e9ba8dd19381333078862587187f66
Nicolas Barrandon <nicolas.barrandon@reach5.co>
2021-12-28 16:16:57 +0100

    fixes related to reach5 sdk 7 and android 31

--- src/main/java/co/reachfive/identity/sdk/demo/AuthenticatedActivity.kt
+++ src/main/java/co/reachfive/identity/sdk/demo/AuthenticatedActivity.kt
@@ -49,10 +49,10 @@ class AuthenticatedActivity : AppCompatActivity() {
 
         devicesBinding = WebauthnDevicesBinding.inflate(layoutInflater)
 
-        this.authToken = intent.getParcelableExtra(AUTH_TOKEN)
+        this.authToken = intent.getParcelableExtra(AUTH_TOKEN)!!
         this.devicesDisplayed = listOf()
 
-        val sdkConfig = intent.getParcelableExtra<SdkConfig>(SDK_CONFIG)
+        val sdkConfig = intent.getParcelableExtra<SdkConfig>(SDK_CONFIG)!!
         this.reach5 = ReachFive(
             sdkConfig = sdkConfig,
             providersCreators = listOf(),
